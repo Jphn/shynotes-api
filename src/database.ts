132818092393e370eebeaaecb0b0c8a135784b6c
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-28 20:29:16 -0300

    refactor: database structure

--- src/database.ts
+++ src/database.ts
@@ -5,4 +5,6 @@ const client = new MongoClient();
 await client.connect(Deno.env.get('MONGO_URL') ?? 'mongodb://127.0.0.1:27017/')
 	.then(() => console.log('> [database.ts] Successfully connected...'));
 
-export { client };
+const database = client.database(Deno.env.get('MONGO_DATABASE') ?? 'shynotes');
+
+export { client, database };
--- src/repositories/implementations/MongoNotesRepository.ts
+++ src/repositories/implementations/MongoNotesRepository.ts
@@ -1,16 +1,9 @@
 import Note from '../../entities/Note.ts';
 import INotesRepository from '../INotesRepository.ts';
-import { Collection, MongoClient } from 'mongo';
+import { database } from '../../database.ts';
 
 export class MongoNotesRepository implements INotesRepository {
-	private notesCollection: Collection<Note>;
-
-	public constructor(client: MongoClient) {
-		this.notesCollection = client.database(Deno.env.get('MONGO_DATABASE'))
-			.collection<Note>(
-				'notes',
-			);
-	}
+	private notesCollection = database.collection<Note>('notes');
 
 	public async findByNoteName(name: string): Promise<Note | undefined> {
 		return await this.notesCollection.findOne({ name: name });
