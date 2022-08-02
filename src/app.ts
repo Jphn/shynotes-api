ead54ef2cf46a9e3025f41c6e212093a6e412a8c
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-08-02 10:02:49 -0300

    refactor: dotenv as first import

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import { Application } from 'oak';
-import router from './routes.ts';
 import 'dotenv';
+import { Application } from 'oak';
+import { router } from './routes.ts';
 
 const app = new Application();
 
