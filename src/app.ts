25bfa78299fdde765e988db388a0ef552a54b50e
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-08-23 20:28:53 -0300

    feat: oakCors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import 'dotenv';
 import { Application } from 'oak';
+import { oakCors } from 'cors';
 import { router } from './routes.ts';
 
 const app = new Application();
+
+app.use(oakCors({
+	origin: '*',
+}));
 
 app.use(router.routes());
 app.use(router.allowedMethods());
